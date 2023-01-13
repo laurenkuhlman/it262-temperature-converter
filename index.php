5f3ba896f6f7e697939e9b2a33ac36c9db7949a8
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-01-12 16:54:25 -0800

    changed temperature selection type
    Made all user input sticky and added a reset button to quickly clear the form.

--- index.php
+++ index.php
@@ -2,44 +2,50 @@
 <html>
 
 <head>
-  <style>
-    * {
-      background-color: beige
-    }
-  </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Temperature Converter</title>
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
 </head>
 
 <body>
   <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <h2>Temperature Converter</h2>
 
     <fieldset>
-      <label>Temperature to Convert from</label>
+      <label>Temperature to Convert</label>
       <input type="number" name="temp_from" value="<?php if (isset($_POST['temp_from'])) echo htmlspecialchars($_POST['temp_from']); ?>" />
-    </fieldset>
 
-    <fieldset>
 
-      <label>Temperature Scale</label>
+
+
+      <label>Convert From:</label>
       <select name="scale_from">
 
         <option value="" NULL<?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == NULL) echo 'selected = "unselected"'; ?>>Select one</option>
 
         <option value="f" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'f') echo 'selected = "selected"'; ?>>Fahrenheit</option>
+        <option value="c" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'c') echo 'selected = "selected"'; ?>>Celsius</option>
+        <option value="k" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'k') echo 'selected = "selected"'; ?>>Kelvin</option>
 
 
 
       </select>
 
-      <label>Temperature Scale</label>
+      <label>Convert To:</label>
       <select name="scale_to">
 
         <option value="" NULL<?php if (isset($_POST['scale_to']) && $_POST['scale_from'] == NULL) echo 'selected = "unselected"'; ?>>Select one</option>
 
-        <option value="c" <?php if (isset($_POST['scale_to']) && $_POST['scale_'] == 'c') echo 'selected = "selected"'; ?>>Celsius</option>
+        <option value="c" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'c') echo 'selected = "selected"'; ?>>Celsius</option>
 
+        <option value="k" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'k') echo 'selected = "selected"'; ?>>Kelvin</option>
 
-
+        <option value="f" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'f') echo 'selected = "selected"'; ?>>Fahrenheit</option>
       </select>
+
+      <a href="" class="reset">Reset</a>
     </fieldset>
     <!-- <fieldset>
       <p>
@@ -88,15 +94,24 @@
     } elseif ($scale_from == "k" && $scale_to == "f") {
       $result = ($temp_from * (9 / 5)) - 459.67;
     } else {
-      echo "Please select a valid conversion type.";
+      echo "<p class='error'>Please complete entire form.</p>";
     }
 
-    if (is_numeric($result)) {
-      echo $temp_from . '°' . strtoupper($scale_from) . ' = ' . round($result, 2) . '°' . strtoupper($scale_to);
-    } else {
-      echo "Please enter a valid number.";
+    if (!empty($temp_from && $scale_from && $scale_to)) {
+      echo '
+    <div class="box">
+      <p>' . $temp_from . '°' . strtoupper($scale_from) . ' = ' . round($result, 2) . '°' . strtoupper($scale_to) . '</p>
+    </div>
+          ';
     }
-    // echo '<pre>';
+
+    // if (is_numeric($result)) {
+    //   echo $temp_from . '°' . strtoupper($scale_from) . ' = ' . round($result, 2) . '°' . strtoupper($scale_to);
+    // } else {
+    //   echo "Please enter a valid number.";
+    // }
+
+    //  echo '<pre>';
     // var_dump ( $_POST );
     // echo '</pre>';
 
